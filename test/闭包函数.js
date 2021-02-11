31e07072bfdafd7b0d0cc67a92a89c2e9c799b90
Weway-git <1419864987@qq.com>
2021-02-11 16:56:32 +0800

    闭包函数

---
+++ test/闭包函数.js
@@ -0,0 +1,13 @@
+function f1(n1,n2,...a){
+    return n1+n2+a[0]+a[1]+a[2]
+}
+
+var u=new User('tom',20)
+var u=new User('tom2',33)
+
+
+function OutFn(){
+    function innerFn(){
+        
+    }
+}
